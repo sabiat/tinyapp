9c23178d5c60e671fa52448ba27fccf3e4f39f52
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-18 22:40:49 +0000

    Added second route for a single URL

--- express_server.js
+++ express_server.js
@@ -23,10 +23,15 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on post ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
