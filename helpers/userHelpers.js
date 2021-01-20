5b686a0f26044fd7793095d7bbc75f3a59b42f72
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-20 22:02:04 +0000

    Added file to hold helper functions

---
+++ helpers/userHelpers.js
@@ -0,0 +1,29 @@
+const generateRandomString = function() {
+  let result = '';
+  let letters = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 6; i++) {
+    result += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
+  return result;
+};
+
+// function to look up if email already exists in database and fetch user
+const fetchUserId = function(userDatabase, email) {
+  for (const user in userDatabase) {
+    if (userDatabase[user]['email'] === email) {
+      return user;
+    }
+  }
+  return false;
+};
+
+const passwordMatch = function(userDatabase, email, password) {
+  for (const user in userDatabase) {
+    if (userDatabase[user]['email'] === email && userDatabase[user]['password'] === password) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = { generateRandomString, fetchUserId, passwordMatch};
